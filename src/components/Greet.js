75b299fdf3515515111c27e5a65839cc13d27061
myonaingwinn <myonaingwin.gic@gmail.com>
2022-06-07 09:11:49 +0630

    functional component

---
+++ src/components/Greet.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const Greet = () => <h2>Hey Jude, Don't make it bad!</h2>;
---
+++ src/components/Greeting.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Greeting = () => {
+    return <h3>Sweet Dreams are made of this!</h3>
+}
+
+export default Greeting;
