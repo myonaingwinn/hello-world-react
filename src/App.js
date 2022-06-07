06308bb9387a0a568b2cb4eb7a21dd6dd195381d
myonaingwinn <myonaingwin.gic@gmail.com>
2022-06-07 09:26:47 +0630

    class component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Welcome from "./components/class_components/Welcome";
 import { Greet } from "./components/functional_components/Greet"; // exact name only
 import GreetingComponent from "./components/functional_components/Greeting"; // can use alias name
 
@@ -7,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Welcome />
         <Greet />
         <GreetingComponent />
       </div>
