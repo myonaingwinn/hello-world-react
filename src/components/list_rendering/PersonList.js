91c82ebb3a2dd907ab97ea755c0238146062dc25
myonaingwinn <myonaingwin.gic@gmail.com>
2022-06-09 11:10:54 +0630

    index as key

--- src/components/list_rendering/PersonList.js
+++ src/components/list_rendering/PersonList.js
@@ -2,8 +2,8 @@ import React from "react";
 import Person from "./Person";
 
 const PersonList = ({ persons }) => {
-  const personList = persons.map((person, index) => (
-    <Person key={person.id} person={person} />
+  const personList = persons.map((person) => (
+    <Person key={person.id} person={person} /> // Don't use 'index' as 'key' prop!!!! Cuz' the list will never be reordered or filtered
   ));
 
   return (
